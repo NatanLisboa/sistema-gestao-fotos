416fb1ca8eb9ad2324a4c81051e13d960737d8b8
unknown <natanflisboa1@gmail.com>
2021-04-06 01:31:19 -0300

    Adiciona API. Injeta HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  photos = [];
+
+  constructor(http: HttpClient){
+    console.log(http);
+  }
+
 }
